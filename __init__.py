0f2e6fd72cf91d195783b628df09304f11e6de12
mister-proger <84221561+mister-proger@users.noreply.github.com>
2023-05-10 12:00:31 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,6 @@
 from YMOperator import YMClient
 from AOperator import Player
 
-import time
+play = Player(input())
 
-#client = YMClient('')
-
-# name = '23500213'
-
-# client.download_track(name, name = name)
-
-play = Player(f'./TEMP/23500213.mp3')
-
-play.async_play()
-
-del play
+play.play()
